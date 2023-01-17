16f841996017628676ce15d3d3397b6d0e0ea2dc
macaulam <119492189+macaulam@users.noreply.github.com>
2023-01-17 15:24:38 +0000

    js changes to event listener to generate button

--- script.js
+++ script.js
@@ -9,33 +9,36 @@ var lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
 
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-  // User choices defaults
-  var pwLength = 0;
-  var spcChar = true;
-  var numChar = true;
-  var uC = true;
-  var lC = true;
+
+// User choices defaults
+var pwLength = 0;
+var spcChar = true;
+var numChar = true;
+var upperChar = true;
+var lowerChar = true;
+
+//variable to store randomly generated characters
+var generatedPassword = [];
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-    pwLength = parseInt(prompt("How long would you like your password to be? Password must be 6 and 18 letters long!"));
-    while (pwLength<6 || pwLength>18){
-    alert("Number must be between 6 and 18, please enter a new number.");
-    getPasswordOptions();
-    }
-    function pwOptions() {
-      spcChar = confirm("Would you like your password to have special characters?");
-      numChar = confirm("Would you like your password to have numerical characters?");
-      uC = confirm("Would you like your password to have upper case letters?");
-      lC = confirm("Would you like your password to have lower case letters?");
-      if (spcChar !== true && numChar !== true && uC !== true &&lC !== true) {
-        alert("Please select at least one character type.");
-        pwOptions();
-      } else {}
-    }
-    pwOptions();
+  pwLength = parseInt(prompt("How long would you like your password to be? Password must be 6 and 18 letters long!"));
+  while (pwLength<6 || pwLength>18){
+  alert("Number must be between 6 and 18, please enter a new number.");
+  getPasswordOptions();
   }
-  
+  function pwOptions() {
+    spcChar = confirm("Would you like your password to have special characters?");
+    numChar = confirm("Would you like your password to have numerical characters?");
+    upperChar = confirm("Would you like your password to have upper case letters?");
+    lowerChar = confirm("Would you like your password to have lower case letters?");
+    if (spcChar !== true && numChar !== true && upperChar !== true &&lowerChar !== true) {
+      alert("Please select at least one character type.");
+      pwOptions();
+    } else {}
+  }
+  pwOptions();
+}
 
 // Function for getting a random element from an array
 function getRandom(a) {
@@ -46,77 +49,77 @@ function getRandom(a) {
 
 // Function to generate password with user input
 function generatePassword() {
-  if (spcChar == true && numChar !== true && uC !== true && lC !== true) {
+  if (spcChar == true && numChar !== true && upperChar !== true && lowerChar !== true) {
     console.log("selected spcChar");
     while (generatedPassword.length < pwLength)
     getRandom(specialCharacters);
 
-    } else if (spcChar !== true && numChar == true && uC !== true && lC !== true) {
+    } else if (spcChar !== true && numChar == true && upperChar !== true && lowerChar !== true) {
       console.log("selected numChar");
       while (generatedPassword.length < pwLength)
       getRandom(numericCharacters);
 
-    } else if (spcChar !== true && numChar !== true && uC == true && lC !== true) {
-      console.log("selected uC");
+    } else if (spcChar !== true && numChar !== true && upperChar == true && lowerChar !== true) {
+      console.log("selected upperChar");
       while (generatedPassword.length < pwLength)
       getRandom(upperCasedCharacters);
 
-    } else if (spcChar !== true && numChar !== true && uC !== true && lC == true) {
-      console.log("selected lC");
+    } else if (spcChar !== true && numChar !== true && upperChar !== true && lowerChar == true) {
+      console.log("selected lowerChar");
       while (generatedPassword.length < pwLength)
       getRandom(lowerCasedCharacters);
       
-    } else if (spcChar == true && numChar == true && uC !== true && lC !== true) {
+    } else if (spcChar == true && numChar == true && upperChar !== true && lowerChar !== true) {
       console.log("selected spcChar & numChar");
       while (generatedPassword.length < pwLength)
       getRandom(specialCharacters + numericCharacters);
 
-    } else if (spcChar == true && numChar !== true && uC == true && lC !== true) {
-      console.log("selected spcChar & uC");
+    } else if (spcChar == true && numChar !== true && upperChar == true && lowerChar !== true) {
+      console.log("selected spcChar & upperChar");
       while (generatedPassword.length < pwLength)
       getRandom(specialCharacters + upperCasedCharacters);
 
-    } else if (spcChar == true && numChar !== true && uC !== true && lC == true) {
-      console.log("selected spcChar & lC");
+    } else if (spcChar == true && numChar !== true && upperChar !== true && lowerChar == true) {
+      console.log("selected spcChar & lowerChar");
       while (generatedPassword.length < pwLength)
       getRandom(specialCharacters + lowerCasedCharacters);
 
-    } else if (spcChar !== true && numChar == true && uC == true && lC !== true) {
-      console.log("selected numChar & uC");
+    } else if (spcChar !== true && numChar == true && upperChar == true && lowerChar !== true) {
+      console.log("selected numChar & upperChar");
       while (generatedPassword.length < pwLength)
       getRandom(numericCharacters + upperCasedCharacters);
 
-    } else if (spcChar !== true && numChar == true && uC !== true && lC == true) {
-      console.log("selected numChar & lC");
+    } else if (spcChar !== true && numChar == true && upperChar !== true && lowerChar == true) {
+      console.log("selected numChar & lowerChar");
       while (generatedPassword.length < pwLength)
       getRandom(numericCharacters + lowerCasedCharacters);
 
-    } else if (spcChar !== true && numChar !== true && uC == true && lC == true) {
-      console.log("selected lC & uC");
+    } else if (spcChar !== true && numChar !== true && upperChar == true && lowerChar == true) {
+      console.log("selected lowerChar & upperChar");
       while (generatedPassword.length < pwLength)
       getRandom(lowerCasedCharacters + upperCasedCharacters);
 
-    }  else if (spcChar == true && numChar == true && uC == true && lC !== true) {
-      console.log("selected spcChar & numChar & uC");
+    }  else if (spcChar == true && numChar == true && upperChar == true && lowerChar !== true) {
+      console.log("selected spcChar & numChar & upperChar");
       while (generatedPassword.length < pwLength)
       getRandom(specialCharacters + numericCharacters + upperCasedCharacters);   
 
-    } else if (spcChar == true && numChar == true && uC !== true && lC == true) {
-      console.log("selected spcChar & numChar & lC");
+    } else if (spcChar == true && numChar == true && upperChar !== true && lowerChar == true) {
+      console.log("selected spcChar & numChar & lowerChar");
       while (generatedPassword.length < pwLength)
       getRandom(specialCharacters + numericCharacters + lowerCasedCharacters);
 
-    } else if (spcChar == true && numChar !== true && uC == true && lC == true) {
-      console.log("selected spcChar & uC & lC");
+    } else if (spcChar == true && numChar !== true && upperChar == true && lowerChar == true) {
+      console.log("selected spcChar & upperChar & lowerChar");
       while (generatedPassword.length < pwLength)
       getRandom(specialCharacters + upperCasedCharacters + lowerCasedCharacters);
 
-    }  else if (spcChar !== true && numChar == true && uC == true && lC == true) {
-      console.log("selected numChar & uC & lC");
+    }  else if (spcChar !== true && numChar == true && upperChar == true && lowerChar == true) {
+      console.log("selected numChar & upperChar & lowerChar");
       while (generatedPassword.length < pwLength)
       getRandom(numericCharacters + upperCasedCharacters + lowerCasedCharacters);
     } else {
-      console.log("selected spcChar & numChar & uC & lC");
+      console.log("selected spcChar & numChar & upperChar & lowerChar");
       while (generatedPassword.length < pwLength)
       getRandom(specialCharacters + numericCharacters + upperCasedCharacters + lowerCasedCharacters);
     }
@@ -125,17 +128,18 @@ function generatePassword() {
   }
 
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
-
+  password = generatePassword();
+  passwordText = document.querySelector('#password');
   passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener('click',() => {
+  getPasswordOptions();
+  writePassword();
+})
